f58dc4955cc2ba125049b9f01d1d636ee04cb4e5
Amandeep Singh <121692605+iaman29@users.noreply.github.com>
2023-04-09 03:04:23 +0530

    minfile is working

--- minfile.py
+++ minfile.py
@@ -1,18 +1,23 @@
 import pandas as pd
 
 # Define a sample DataFrame with student data and marks
-data2 = {
-    "student_id": [1, 2, 3, 4, 5],
-    "name": ["Alice", "Bob", "Charlie", "Dave", "Eve"],
-    "email": ["alice@example.com", "bob@example.com", "charlie@example.com", "dave@example.com", "eve@example.com"],
-    "computer": [60, 70, 80, 90, 65],
-    "mechanics": [80, 75, 70, 60, 85],
-    "maths": [90, 80, 70, 60, 50],
-    "English": [70, 75, 80, 85, 90],
-    "electrical": [65, 60, 55, 70, 75],
-    "physics": [85, 90, 95, 80, 75],
-    "chemistry": [75, 80, 85, 90, 95]
-}
+
+
+# -----------------------------------------SAMPLE DATA NOT FOR USE---------------------------------------
+# data2 = {
+#     "student_id": [1, 2, 3, 4, 5],
+#     "name": ["Alice", "Bob", "Charlie", "Dave", "Eve"],
+#     "email": ["alice@example.com", "bob@example.com", "charlie@example.com", "dave@example.com", "eve@example.com"],
+#     "computer": [60, 70, 80, 90, 65],
+#     "mechanics": [80, 75, 70, 60, 85],
+#     "maths": [90, 80, 70, 60, 50],
+#     "English": [70, 75, 80, 85, 90],
+#     "electrical": [65, 60, 55, 70, 75],
+#     "physics": [85, 90, 95, 80, 75],
+#     "chemistry": [75, 80, 85, 90, 95]
+# 
+# }-------------------------------------------------------------------------------------------------------
+# 
 data=pd.read_csv("Book1.csv")
 df = pd.DataFrame(data)
 
